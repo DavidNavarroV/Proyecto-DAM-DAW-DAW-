442cfc578dd89969b5cd75c838ee4856e39a1f13
lusam48 <lusam.48@gmail.com>
2022-02-04 16:09:24 +0100

    primer dia vier 04. creadas las vistas y plan de proyecto

---
+++ ProyectoDAM-DAW/ProyectoDAM-DAW/resources/views/admin/index.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.admin')
+
+@section('content')
+
+@endsection
+<!--
+header: usuarios -> tu nombre_usuario// inicio -> estadisticcas
+listado top 5(derecha)
+IZQUIERDA:
+estadistica ultimas partidas
+total tempo
+total partidas
+puntuacion mas alta
+
+
+
+
+
+-->
